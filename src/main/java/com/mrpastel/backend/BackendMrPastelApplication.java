697b896f0e10399a8d75997fb558a55e3945fc36
Omar F.M. <om.felipe@duocuc.cl>
2025-11-19 20:11:23 -0300

    Fix swagger server

--- src/main/java/com/mrpastel/backend/BackendMrPastelApplication.java
+++ src/main/java/com/mrpastel/backend/BackendMrPastelApplication.java
@@ -2,6 +2,21 @@ package com.mrpastel.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+    info = @Info(
+        title = "API Mr. Pastel",
+        version = "1.0",
+        description = "API REST completa con JWT, CRUD y Base de Datos"
+    ),
+    servers = {
+        // Usar siempre el mismo host desde donde se carga Swagger
+        @Server(url = "/", description = "Servidor actual")
+    }
+)
 
 @SpringBootApplication
 public class BackendMrPastelApplication {
